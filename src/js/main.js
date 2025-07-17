0d175af4a12cf3f09528922ab3cd309dbffe99a5
Linder, Andreas <ego@linder-andreas.de>
2025-07-17 14:08:57 +0200

    JavaScript modules test

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import { fetchData, apiKey, config} from './module.js';
+
+console.log(apiKey);
+console.log(fetchData());
+console.log(config.timeout);
---
+++ src/js/module.js
@@ -0,0 +1,11 @@
+export const apiKey = 'abc123';
+
+export function fetchData() {
+    return 'data';
+}
+
+const config = {
+    timeout: 5000
+};
+
+export {config}; // Named export
